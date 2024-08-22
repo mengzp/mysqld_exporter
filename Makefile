67ceefd7ca966394fd47037613cdab8512499bef
mengzp <405168057@qq.com>
2024-08-22 11:23:08 +0800

    first:

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Copyright 2013 The Prometheus Authors
+# Copyright 2015 The Prometheus Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -11,7 +11,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-VERSION  := 0.1.0
-TARGET   := mysqld_exporter
+# Needs to be defined before including Makefile.common to auto-generate targets
+DOCKER_ARCHS ?= amd64 armv7 arm64
 
-include Makefile.COMMON
+all: vet
+
+include Makefile.common
+
+STATICCHECK_IGNORE =
+
+DOCKER_IMAGE_NAME ?= mysqld-exporter
+
+.PHONY: test-docker-single-exporter
+test-docker-single-exporter:
+	@echo ">> testing docker image for single exporter"
+	./test_image.sh "$(DOCKER_IMAGE_NAME):$(DOCKER_IMAGE_TAG)" 9104
+
+.PHONY: test-docker
--- mysqld_exporter.go
+++ mysqld_exporter.go
@@ -1,191 +1,293 @@
+// Copyright 2018 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"encoding/json"
-	"flag"
-	"io/ioutil"
-	"log"
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 	"strconv"
-	"sync"
+	"time"
 
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-
+	"github.com/alecthomas/kingpin/v2"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
-)
+	versioncollector "github.com/prometheus/client_golang/prometheus/collectors/version"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"github.com/prometheus/common/version"
+	"github.com/prometheus/exporter-toolkit/web"
+	webflag "github.com/prometheus/exporter-toolkit/web/kingpinflag"
 
-const (
-	namespace = "mysql"
+	"github.com/prometheus/mysqld_exporter/collector"
+	"github.com/prometheus/mysqld_exporter/config"
 )
 
 var (
-	listenAddress = flag.String("web.listen-address", ":9091", "Address to listen on for web interface and telemetry.")
-	metricPath    = flag.String("web.telemetry-path", "/metrics", "Path under which to expose metrics.")
-	configFile    = flag.String("config.file", "mysqld_exporter.conf", "Path to config file.")
+	metricsPath = kingpin.Flag(
+		"web.telemetry-path",
+		"Path under which to expose metrics.",
+	).Default("/metrics").String()
+	timeoutOffset = kingpin.Flag(
+		"timeout-offset",
+		"Offset to subtract from timeout in seconds.",
+	).Default("0.25").Float64()
+	configMycnf = kingpin.Flag(
+		"config.my-cnf",
+		"Path to .my.cnf file to read MySQL credentials from.",
+	).Default(".my.cnf").String()
+	mysqldAddress = kingpin.Flag(
+		"mysqld.address",
+		"Address to use for connecting to MySQL",
+	).Default("localhost:3306").String()
+	mysqldUser = kingpin.Flag(
+		"mysqld.username",
+		"Hostname to use for connecting to MySQL",
+	).String()
+	tlsInsecureSkipVerify = kingpin.Flag(
+		"tls.insecure-skip-verify",
+		"Ignore certificate and server verification when using a tls connection.",
+	).Bool()
+	toolkitFlags = webflag.AddFlags(kingpin.CommandLine, ":9104")
+	c            = config.MySqlConfigHandler{
+		Config: &config.Config{},
+	}
 )
 
-type Config struct {
-	Config map[string]string `json:"config"`
+// scrapers lists all possible collection methods and if they should be enabled by default.
+var scrapers = map[collector.Scraper]bool{
+	collector.ScrapeGlobalStatus{}:                        true,
+	collector.ScrapeGlobalVariables{}:                     true,
+	collector.ScrapeSlaveStatus{}:                         true,
+	collector.ScrapeProcesslist{}:                         false,
+	collector.ScrapeUser{}:                                false,
+	collector.ScrapeTableSchema{}:                         false,
+	collector.ScrapeInfoSchemaInnodbTablespaces{}:         false,
+	collector.ScrapeInnodbMetrics{}:                       false,
+	collector.ScrapeAutoIncrementColumns{}:                false,
+	collector.ScrapeBinlogSize{}:                          false,
+	collector.ScrapePerfTableIOWaits{}:                    false,
+	collector.ScrapePerfIndexIOWaits{}:                    false,
+	collector.ScrapePerfTableLockWaits{}:                  false,
+	collector.ScrapePerfEventsStatements{}:                false,
+	collector.ScrapePerfEventsStatementsSum{}:             false,
+	collector.ScrapePerfEventsWaits{}:                     false,
+	collector.ScrapePerfFileEvents{}:                      false,
+	collector.ScrapePerfFileInstances{}:                   false,
+	collector.ScrapePerfMemoryEvents{}:                    false,
+	collector.ScrapePerfReplicationGroupMembers{}:         false,
+	collector.ScrapePerfReplicationGroupMemberStats{}:     false,
+	collector.ScrapePerfReplicationApplierStatsByWorker{}: false,
+	collector.ScrapeSysUserSummary{}:                      false,
+	collector.ScrapeUserStat{}:                            false,
+	collector.ScrapeClientStat{}:                          false,
+	collector.ScrapeTableStat{}:                           false,
+	collector.ScrapeSchemaStat{}:                          false,
+	collector.ScrapeInnodbCmp{}:                           true,
+	collector.ScrapeInnodbCmpMem{}:                        true,
+	collector.ScrapeQueryResponseTime{}:                   true,
+	collector.ScrapeEngineTokudbStatus{}:                  false,
+	collector.ScrapeEngineInnodbStatus{}:                  false,
+	collector.ScrapeHeartbeat{}:                           false,
+	collector.ScrapeSlaveHosts{}:                          false,
+	collector.ScrapeReplicaHost{}:                         false,
+	collector.ScrapeSchemaSizeSchema{}:                    false,
+	collector.ScrapeBackupStatSchema{}:                    true,
 }
 
-type Exporter struct {
-	config                     Config
-	mutex                      sync.RWMutex
-	up                         prometheus.Gauge
-	totalScrapes, errorScrapes prometheus.Counter
-	metrics                    map[string]prometheus.Gauge
-}
+func filterScrapers(scrapers []collector.Scraper, collectParams []string) []collector.Scraper {
+	var filteredScrapers []collector.Scraper
 
-func getConfig(file string) (*Config, error) {
-	config := &Config{}
+	// Check if we have some "collect[]" query parameters.
+	if len(collectParams) > 0 {
+		filters := make(map[string]bool)
+		for _, param := range collectParams {
+			filters[param] = true
+		}
 
-	bytes, err := ioutil.ReadFile(file)
-	if err != nil {
-		return nil, err
+		for _, scraper := range scrapers {
+			if filters[scraper.Name()] {
+				filteredScrapers = append(filteredScrapers, scraper)
+			}
+		}
 	}
-
-	return config, json.Unmarshal(bytes, &config)
+	if len(filteredScrapers) == 0 {
+		return scrapers
+	}
+	return filteredScrapers
 }
 
-// return new empty exporter
-func NewMySQLExporter(config *Config) *Exporter {
-	return &Exporter{
-		config: *config,
-		up: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "up",
-			Help:      "Was the last scrape of mysqld successful.",
-		}),
-		totalScrapes: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: namespace,
-			Name:      "exporter_total_scrapes",
-			Help:      "Current total mysqld scrapes.",
-		}),
-		errorScrapes: prometheus.NewCounter(prometheus.CounterOpts{
-			Namespace: namespace,
-			Name:      "exporter_error_scrapes",
-			Help:      "Error mysqld scrapes.",
-		}),
-		metrics: map[string]prometheus.Gauge{},
-	}
-}
-
-func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
-	for _, m := range e.metrics {
-		m.Describe(ch)
-	}
-
-	ch <- e.up.Desc()
-	ch <- e.totalScrapes.Desc()
-	ch <- e.errorScrapes.Desc()
-}
-
-func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	scrapes := make(chan []string)
-
-	go e.scrape(scrapes)
-
-	e.mutex.Lock()
-	defer e.mutex.Unlock()
-	e.setMetrics(scrapes)
-	ch <- e.up
-	ch <- e.totalScrapes
-	ch <- e.errorScrapes
-	e.collectMetrics(ch)
-}
-
-func (e *Exporter) scrape(scrapes chan<- []string) {
-	defer close(scrapes)
-
-	e.totalScrapes.Inc()
-	e.up.Set(0)
-
-	db, err := sql.Open("mysql", e.config.Config["mysql_connection"])
-	if err != nil {
-		log.Printf("error open connection to db using %s", e.config.Config["mysql_connection"])
-		return
-	}
-	defer db.Close()
-
-	rows, err := db.Query("SHOW STATUS")
-	if err != nil {
-		log.Printf("error running query on db %s", e.config.Config["mysql_connection"])
-		return
-	}
-	defer rows.Close()
-
-	e.up.Set(1) // from this point db is ok
-
-	var key, val []byte
-	for rows.Next() {
-		// get RawBytes from data
-		err = rows.Scan(&key, &val)
+func getScrapeTimeoutSeconds(r *http.Request, offset float64) (float64, error) {
+	var timeoutSeconds float64
+	if v := r.Header.Get("X-Prometheus-Scrape-Timeout-Seconds"); v != "" {
+		var err error
+		timeoutSeconds, err = strconv.ParseFloat(v, 64)
 		if err != nil {
-			log.Printf("error getting result set ")
-			return
+			return 0, fmt.Errorf("failed to parse timeout from Prometheus header: %v", err)
 		}
-
-		var res []string = make([]string, 2)
-		res[0] = string(key)
-		res[1] = string(val)
-
-		scrapes <- res
+	}
+	if timeoutSeconds == 0 {
+		return 0, nil
+	}
+	if timeoutSeconds < 0 {
+		return 0, fmt.Errorf("timeout value from Prometheus header is invalid: %f", timeoutSeconds)
 	}
 
-	return
+	if offset >= timeoutSeconds {
+		// Ignore timeout offset if it doesn't leave time to scrape.
+		return 0, fmt.Errorf("timeout offset (%f) should be lower than prometheus scrape timeout (%f)", offset, timeoutSeconds)
+	} else {
+		// Subtract timeout offset from timeout.
+		timeoutSeconds -= offset
+	}
+	return timeoutSeconds, nil
 }
 
-func (e *Exporter) setMetrics(scrapes <-chan []string) {
-	for row := range scrapes {
+func init() {
+	prometheus.MustRegister(versioncollector.NewCollector("mysqld_exporter"))
+}
 
-		name := row[0]
-		value, err := strconv.ParseInt(row[1], 10, 64)
+func newHandler(scrapers []collector.Scraper, logger log.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var dsn string
+		var err error
+		target := ""
+		q := r.URL.Query()
+		if q.Has("target") {
+			target = q.Get("target")
+		}
+
+		cfg := c.GetConfig()
+		cfgsection, ok := cfg.Sections["client"]
+		if !ok {
+			level.Error(logger).Log("msg", "Failed to parse section [client] from config file", "err", err)
+		}
+		if dsn, err = cfgsection.FormDSN(target); err != nil {
+			level.Error(logger).Log("msg", "Failed to form dsn from section [client]", "err", err)
+		}
+
+		collect := q["collect[]"]
+
+		// Use request context for cancellation when connection gets closed.
+		ctx := r.Context()
+		// If a timeout is configured via the Prometheus header, add it to the context.
+		timeoutSeconds, err := getScrapeTimeoutSeconds(r, *timeoutOffset)
 		if err != nil {
-			// not really important
-			// log.Printf("Error while parsing field value %s (%s): %v", row[0], row[1], err)
-			e.errorScrapes.Inc()
-			continue
+			level.Error(logger).Log("msg", "Error getting timeout from Prometheus header", "err", err)
+		}
+		if timeoutSeconds > 0 {
+			// Create new timeout context with request context as parent.
+			var cancel context.CancelFunc
+			ctx, cancel = context.WithTimeout(ctx, time.Duration(timeoutSeconds*float64(time.Second)))
+			defer cancel()
+			// Overwrite request with timeout context.
+			r = r.WithContext(ctx)
 		}
 
-		if _, ok := e.metrics[name]; !ok {
-			e.metrics[name] = prometheus.NewGauge(prometheus.GaugeOpts{
-				Namespace: namespace,
-				Name:      name,
-			})
+		filteredScrapers := filterScrapers(scrapers, collect)
+
+		registry := prometheus.NewRegistry()
+
+		registry.MustRegister(collector.New(ctx, dsn, filteredScrapers, logger))
+
+		gatherers := prometheus.Gatherers{
+			prometheus.DefaultGatherer,
+			registry,
 		}
-
-		e.metrics[name].Set(float64(value))
-	}
-}
-
-func (e *Exporter) collectMetrics(metrics chan<- prometheus.Metric) {
-	for _, m := range e.metrics {
-		m.Collect(metrics)
+		// Delegate http serving to Prometheus client library, which will call collector.Collect.
+		h := promhttp.HandlerFor(gatherers, promhttp.HandlerOpts{})
+		h.ServeHTTP(w, r)
 	}
 }
 
 func main() {
-	flag.Parse()
+	// Generate ON/OFF flags for all scrapers.
+	scraperFlags := map[collector.Scraper]*bool{}
+	for scraper, enabledByDefault := range scrapers {
+		defaultOn := "false"
+		if enabledByDefault {
+			defaultOn = "true"
+		}
 
-	config, err := getConfig(*configFile)
-	if err != nil {
-		log.Fatal("couldn't read config file ", *configFile, " : ", err)
+		f := kingpin.Flag(
+			"collect."+scraper.Name(),
+			scraper.Help(),
+		).Default(defaultOn).Bool()
+
+		scraperFlags[scraper] = f
 	}
 
-	exporter := NewMySQLExporter(config)
-	prometheus.MustRegister(exporter)
-	http.Handle(*metricPath, prometheus.Handler())
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
-<head><title>MySQLd exporter</title></head>
-<body>
-<h1>MySQLd exporter</h1>
-<p><a href='` + *metricPath + `'>Metrics</a></p>
-</body>
-</html>
-`))
-	})
+	// Parse flags.
+	promlogConfig := &promlog.Config{}
+	flag.AddFlags(kingpin.CommandLine, promlogConfig)
+	kingpin.Version(version.Print("mysqld_exporter"))
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	logger := promlog.New(promlogConfig)
 
-	log.Fatal(http.ListenAndServe(*listenAddress, nil))
+	level.Info(logger).Log("msg", "Starting mysqld_exporter", "version", version.Info())
+	level.Info(logger).Log("msg", "Build context", "build_context", version.BuildContext())
+
+	var err error
+	if err = c.ReloadConfig(*configMycnf, *mysqldAddress, *mysqldUser, *tlsInsecureSkipVerify, logger); err != nil {
+		level.Info(logger).Log("msg", "Error parsing host config", "file", *configMycnf, "err", err)
+		os.Exit(1)
+	}
+
+	// Register only scrapers enabled by flag.
+	enabledScrapers := []collector.Scraper{}
+	for scraper, enabled := range scraperFlags {
+		if *enabled {
+			level.Info(logger).Log("msg", "Scraper enabled", "scraper", scraper.Name())
+			enabledScrapers = append(enabledScrapers, scraper)
+		}
+	}
+	handlerFunc := newHandler(enabledScrapers, logger)
+	http.Handle(*metricsPath, promhttp.InstrumentMetricHandler(prometheus.DefaultRegisterer, handlerFunc))
+	if *metricsPath != "/" && *metricsPath != "" {
+		landingConfig := web.LandingConfig{
+			Name:        "MySQLd Exporter",
+			Description: "Prometheus Exporter for MySQL servers",
+			Version:     version.Info(),
+			Links: []web.LandingLinks{
+				{
+					Address: *metricsPath,
+					Text:    "Metrics",
+				},
+			},
+		}
+		landingPage, err := web.NewLandingPage(landingConfig)
+		if err != nil {
+			level.Error(logger).Log("err", err)
+			os.Exit(1)
+		}
+		http.Handle("/", landingPage)
+	}
+	http.HandleFunc("/probe", handleProbe(enabledScrapers, logger))
+	http.HandleFunc("/-/reload", func(w http.ResponseWriter, r *http.Request) {
+		if err = c.ReloadConfig(*configMycnf, *mysqldAddress, *mysqldUser, *tlsInsecureSkipVerify, logger); err != nil {
+			level.Warn(logger).Log("msg", "Error reloading host config", "file", *configMycnf, "error", err)
+			return
+		}
+		_, _ = w.Write([]byte(`ok`))
+	})
+	srv := &http.Server{}
+	if err := web.ListenAndServe(srv, toolkitFlags, logger); err != nil {
+		level.Error(logger).Log("msg", "Error starting HTTP server", "err", err)
+		os.Exit(1)
+	}
 }
